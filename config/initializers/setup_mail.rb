ad2fe47b92bef3fd6ad160dc8122c314e0c2e9de
claudiamaryory <claudiamaryory@hotmail.com>
2013-11-11 15:45:06 -0500

    enzayo de diploy en heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
   :address  => "smtp.gmail.com",
   :port     => 587,
   :domain   => "local",
-  :user_name => "claudiamaryory@misena.edu.co",
-  :password => "zamircerezo",
+  :user_name => "claudiamaryory@misena.edu.co",#aca coloco el correo de la empresa
+  :password => "zamircerezo",#clave del correo de la empresa
   :authentication => "plain",
   :enable_starttls_auto => true
 
 }
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+  ActionMailer::Base.default_url_options[:host] = "sah.herokuapp.com"
